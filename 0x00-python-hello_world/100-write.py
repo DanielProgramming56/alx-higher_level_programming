33679233582db3a23fbb005f98508a39e2b84214
Daniel Chidebere <daniel.chidebere@konga.com>
2023-10-03 13:42:41 +0100

    Edited(2-end)

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+
+# Write the message to stderr
+sys.stderr.write(message + "\n")
+
+# Exit with status code 1
+sys.exit(1)
