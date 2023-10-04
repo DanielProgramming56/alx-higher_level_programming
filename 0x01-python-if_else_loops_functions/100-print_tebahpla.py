93ad6fe68ddc5abb623d25896a8284bf50fd4a5f
Daniel Chidebere <daniel.chidebere@konga.com>
2023-10-04 14:48:44 +0100

    fix(file-100)

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for char in range(ord('z'), ord('A') - 1, -1):
-    print("{:c}".format(char), end='')
+    if char % 2 == 0:
+        print("{:c}".format(char), end='')
+    else:
+        print("{:c}".format(char - 32), end='')
