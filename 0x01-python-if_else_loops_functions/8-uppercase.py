206f571e38ecbded3b1b6426e1ca95cfe3e1d165
Daniel Chidebere <daniel.chidebere@konga.com>
2023-10-04 14:17:04 +0100

    doneIfile-8)

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:
+        if 'a' <= char <= 'z':
+            uppercase_char = chr(ord(char) - 32)
+            print(uppercase_char, end='')
+        else:
+            print(char, end='')
