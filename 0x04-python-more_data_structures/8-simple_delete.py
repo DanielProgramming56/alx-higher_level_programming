961787a86ea4da43fc26fca9ec4a522c86da363a
Daniel Chidebere <daniel.chidebere@konga.com>
2023-10-12 16:43:18 +0100

    fix(file-8)

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    # Check if the key exists in the dictionary
     if key in a_dictionary:
-        # If the key exists, delete it
         del a_dictionary[key]
+    return a_dictionary
