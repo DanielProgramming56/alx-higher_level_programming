0e4fd504087aa658dc40dfbe3b8a12033f7e7ee9
Daniel Chidebere <daniel.chidebere@konga.com>
2023-10-12 10:24:31 +0100

    fix(file-4)

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return set_1 | set_2
+    difference_1 = set_1 - set_2
+    difference_2 = set_2 - set_1
+    result_set = difference_1.union(difference_2)
+    return result_set
