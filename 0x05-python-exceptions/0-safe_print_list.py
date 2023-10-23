98a87497204cd439a7dca4a355de94442ebf1ede
daniel.chidebere <daniel.chidebere@konga.com>
2023-10-23 19:57:15 +0100

    fix(file-0)

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,13 @@
-#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    for value in my_list:
-        try:
-            print(value, end='')
-            count += 1
-
-            if count == x:
-                break
-
-        except:
-            continue
-    print()
-
-    return count
+    i = 0
+    if x == 0:
+        print()
+        return i
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+        print()
+    except IndexError:
+        print()
+        return i
+    return i + 1
