290e5f4aa02bac46a52ed6f0a098e1959cbea000
daniel.chidebere <daniel.chidebere@konga.com>
2023-11-08 07:25:37 +0100

    fix(file-0)

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """Reads and prints content of file"""
-    with open(filename, 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content)
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
