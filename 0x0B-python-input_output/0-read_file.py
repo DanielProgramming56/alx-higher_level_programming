ff9dd9b8b7c9167acf0e6886b8b98cb77b6b802c
daniel.chidebere <daniel.chidebere@konga.com>
2023-11-08 07:23:18 +0100

    fix(file-0)

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+"""Contain a single function that read files"""
+
+
 def read_file(filename=""):
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content)
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-    except Exception as e:
-        print(f"An Error occurred: {e}")
+    """Reads and prints content of file"""
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+        print(content)
