e46f96a3903ea2bbde14c03e87c039c892dd277d
pixelharbor-tech <pixelharbor0@gmail.com>
2024-01-09 16:37:14 +0100

    done

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+if (isNaN(argv[2])) {
+    console.log('Missing number of occurrences');
+} else {
+    if (parseInt(argv[2]) !== 0) {
+        for (let i = 0; i < argv[2]; i++) {
+            console.log('C is fun');
+        }
+    }
+}
