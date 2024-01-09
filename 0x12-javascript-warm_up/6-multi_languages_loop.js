3a34e6bfe5044f854df01905051e67ab79dfac34
pixelharbor-tech <pixelharbor0@gmail.com>
2024-01-09 16:22:42 +0100

    done

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-let i = 0;
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-while (i != 3) {
-    console.log(arr[i]);
-    i++;
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < strings.length; i++) {
+    console.log(strings[i]);
 }
