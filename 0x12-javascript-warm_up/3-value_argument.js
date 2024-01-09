50cc249208af03b813f1f9efef55b2a43bdf8a3f
pixelharbor-tech <pixelharbor0@gmail.com>
2024-01-09 16:03:34 +0100

    done

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('process')
+const { argv } = require('process');
 
-const argvLength = argv.length - 2;
-if (argvLength === 0) {
-    console.log("No argument");
+if (argv[2] === undefined) {
+    console.log('No argument');
 } else {
-    console.log(argv[argvLength]);
+    console.log(argv[2]);
 }
